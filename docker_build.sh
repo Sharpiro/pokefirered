9a9e284c3095bcb4db4d48c5299d3c32807665c8
sharpiro <dsharpbb09@gmail.com>
2021-06-13 18:28:13 -0400

    add build scripts

---
+++ docker_build.sh
@@ -0,0 +1,9 @@
+set -e
+
+# docker build -t pokefirered_builder .
+docker run --rm -it -v $PWD:/app/pokefirered pokefirered_builder
+echo -e "\nbuild succeded!"
+echo "default 16777216 41cb23d8dccc8ebd7c649cd8fbb58eeace6e2fdc"
+checksum=`sha1sum pokefirered.gba | cut -d " " -f1`
+size=`stat --format "%s" pokefirered.gba`
+echo "current $size $checksum"
