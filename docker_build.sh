a52fd9d20e4b4ca06dea6c65df250a349ca3952e
sharpiro <dsharpbb09@gmail.com>
2021-07-19 19:16:48 -0400

    build docker image if none available

--- docker_build.sh
+++ docker_build.sh
@@ -1,6 +1,11 @@
+docker inspect pokefirered_builder > /dev/null 2>&1
+if [[ $? != 0 ]]; then
+    echo "building image"
+    docker build -t pokefirered_builder .
+fi
+
 set -e
 
-# docker build -t pokefirered_builder .
 docker run --rm -it -v $PWD:/app/pokefirered pokefirered_builder
 echo -e "\nbuild succeded!"
 echo "default 16777216 41cb23d8dccc8ebd7c649cd8fbb58eeace6e2fdc"
